dff8a8ac9fc1a23d568be05439f0cbba82ea402b
Zach Mommaerts <zmommaerts@gmail.com>
2020-01-27 14:38:55 -0600

    added indexer and commented out certain tests so I can run my first tests

--- CustomList/CustomList/CustomList.cs
+++ CustomList/CustomList/CustomList.cs
@@ -4,17 +4,27 @@ using System.Text;
 
 namespace CustomList
 {
-    class CustomList
+    public class CustomList <T>
     {
         //Member Variables (HAS A)
+        T[] items;
+        int capacity;
+        int count;
+
 
         //Constructor
         public CustomList()
         {
-
+            items = new T[capacity];
         }
         //Member Methods (CAN DO)
-
+        public void Add(T itemToAdd)
+        {
+            for (int i = 0; i < items.Length ; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+        }
 
     }
 }
